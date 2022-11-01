51a45986af14ad03daed3ee2348fee6f234413d6
sonviet87 <sonviet87@gmail.com>
2022-11-01 18:03:40 +0700

    fix bug accounts

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AccountCollection;
 use App\Services\AccountService;
 use Illuminate\Http\Request;
 
@@ -24,14 +25,15 @@ class AccountController extends RestfulController
     {
         try {
             $perPage = $request->input("per_page", 20);
-            $product = $this->accountService->getListPaginate($perPage);
-            $product->appends($request->except(['page', '_token']));
-            $paginator = $this->getPaginator($product);
-            $pagingArr = $product->toArray();
-            return $this->_response([
+            $accounts = $this->accountService->getListPaginate($perPage);
+           // $accounts->appends($request->except(['page', '_token']));
+           // $paginator = $this->getPaginator($accounts);
+           // $pagingArr = $accounts->toArray();
+           /* return $this->_response([
                 'pagination' => $paginator,
                 'account' => $pagingArr['data']
-            ]);
+            ]);*/
+            return new AccountCollection($accounts);
         } catch (\Exception $e) {
             return $this->_error($e, self::HTTP_INTERNAL_ERROR);
         }
---
+++ app/Http/Resources/AccountCollection.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AccountCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+
+
+    public function toArray($request)
+    {
+        return $this->collection->transform(function ($page) {
+                return [
+                    'id' => $page->id,
+                    'address' => $page->address,
+                    'city' => $page->city,
+                    'district' => $page->district,
+                    'industry' => $page->industry,
+                    'legal_address' => $page->legal_address,
+                    'legal_name' => $page->legal_name,
+                    'name' => $page->name,
+                    'phone' => $page->phone,
+                ];
+            });
+
+
+    }
+
+    public function with($request)
+    {
+        return [
+            'status' => true,
+        ];
+    }
+}
