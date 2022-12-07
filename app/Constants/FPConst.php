c7ee0f1c0b40f186c7e50a7b23ab949f3eb2c671
sonviet87 <sonviet87@gmail.com>
2022-12-07 17:53:55 +0700

    update fpconst

--- app/Constants/FPConst.php
+++ app/Constants/FPConst.php
@@ -8,9 +8,9 @@ class FPConst
     const STATUS_CONTRACT = 3;
     const STATUS_SHIPPING = 4;
     const STATUS_INVOICE = 5;
-    const STATUS_INVOICE_FAILED = 6;
-    const STATUS_COMPLETED = 7;
-    const STATUS_BACK = 8;
+
+    const STATUS_COMPLETED = 6;
+    const STATUS_BACK = 7;
 
     const STATUS_NAME = [
         FPConst::STATUS_NEW => 'Mới',
@@ -19,7 +19,7 @@ class FPConst
         FPConst::STATUS_CONTRACT => 'Duyệt hợp đồng',
         FPConst::STATUS_SHIPPING => 'Duyệt giao hàng',
         FPConst::STATUS_INVOICE => 'Xuất hóa đơn',
-        FPConst::STATUS_INVOICE_FAILED => 'Hủy hóa đơn',
+
         FPConst::STATUS_COMPLETED => 'Hoàn tất hợp đồng',
         FPConst::STATUS_BACK => 'Trả về',
     ];
